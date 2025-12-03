8b54359c7b90f0e73b6ceb573b04c75c0f0eb3c6
koraynilay <koray.fra@gmail.com>
2025-12-03 23:31:07 +0100

    second puzzle part 2

---
+++ 2025/2/giftshop_part2.hs
@@ -0,0 +1,38 @@
+import Data.List.Split
+
+isInvalid :: Integer -> (Bool,Integer)
+isInvalid x = isInvalidAux x (numLength x `div` 2)
+
+isInvalidAux :: Integer -> Integer -> (Bool,Integer)
+isInvalidAux _ 0 = (False,-1)
+isInvalidAux x n | res && rel = (True,x)
+                 | otherwise = isInvalidAux x (n-1)
+  where
+        baseRem = x `rem` (10^n)
+        res = all (== baseRem) ll
+        rel = all ((== n).numLength) ll
+        ll = makelist n
+        makelist k | d == 0 = []
+                   | otherwise = d `rem` (10^n) : makelist (k+n)
+          where d = x `div` (10^k)
+
+checkRange :: String -> [(Bool,Integer)]
+checkRange s = filter (\(x,y) -> x /= False) $ map isInvalid [start..end]
+  where
+        range = splitOn "-" s
+        start = read (range!!0)::Integer
+        end = read (range!!1)::Integer
+
+numLength :: Integer -> Integer
+numLength x | x < 10 = 1
+            | x < 100 = 2
+            | x < 1000 = 3
+            | x < 10000 = 4
+            | x < 100000 = 5
+            | x < 1000000 = 6
+            | otherwise = 1 + numLength (x `div` 10)
+
+main :: IO ()
+main = do
+    file <- readFile "input.txt"
+    print $ sum $ map snd $ concat $ map checkRange $ splitOn "," file
